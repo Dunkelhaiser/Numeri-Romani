82ff96b5afeb7f7e623aa9a721a105c0dca2075e
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-05-27 15:30:10 +0300

    feat: added addition

--- src/RomanNumber.test.ts
+++ src/RomanNumber.test.ts
@@ -24,4 +24,30 @@ describe("RomanNumber class", () => {
         expect(romanNumber.getNumericValue()).toBe(2);
         expect(romanNumber.getValue()).toBe("II");
     });
+
+    describe("add", () => {
+        it("adds a roman number to the RomanNumber instance", () => {
+            const romanNumber = new RomanNumber(1);
+            romanNumber.add("II");
+            expect(romanNumber.getNumericValue()).toBe(3);
+            expect(romanNumber.getValue()).toBe("III");
+        });
+
+        it("adds a arabic number to the RomanNumber instance", () => {
+            const romanNumber = new RomanNumber(5);
+            romanNumber.add(-2);
+            expect(romanNumber.getNumericValue()).toBe(3);
+            expect(romanNumber.getValue()).toBe("III");
+        });
+
+        it("throws an error if the result is greater than 3999", () => {
+            const romanNumber = new RomanNumber(3998);
+            expect(() => romanNumber.add(2)).toThrowError("The sum of the two values exceeds 3999");
+        });
+
+        it("throws an error if the result is less than 1", () => {
+            const romanNumber = new RomanNumber(1);
+            expect(() => romanNumber.add(-2)).toThrowError("The sum of the two values is less than 1");
+        });
+    });
 });
--- src/RomanNumber.ts
+++ src/RomanNumber.ts
@@ -1,4 +1,4 @@
-import { arabicize, romanize } from "./utils";
+import { arabicize, isValidRoman, romanize } from "./utils";
 
 /**
  * A class representing a roman number.
@@ -51,6 +51,39 @@ export class RomanNumber {
         } else {
             this.numericValue = arabicize(value);
             this.value = value;
+        }
+    }
+
+    /**
+     * Adds a value to the RomanNumber instance.
+     * @param value The value to add. Can be either a arabic number or a roman numeral string.
+     */
+
+    public add(value: number | string) {
+        if (typeof value === "number") {
+            if (!Number.isInteger(value)) throw new Error("The value must be an integer");
+            const tempSum = this.numericValue + value;
+            if (tempSum > 3999) {
+                throw new Error("The sum of the two values exceeds 3999");
+            }
+            if (tempSum < 1) {
+                throw new Error("The sum of the two values is less than 1");
+            }
+
+            this.numericValue = tempSum;
+            this.value = romanize(tempSum);
+        } else {
+            isValidRoman(value);
+            const tempSum = this.numericValue + arabicize(value);
+            if (tempSum > 3999) {
+                throw new Error("The sum of the two values exceeds 3999");
+            }
+            if (tempSum < 1) {
+                throw new Error("The sum of the two values is less than 1");
+            }
+
+            this.numericValue = tempSum;
+            this.value = romanize(tempSum);
         }
     }
 }
